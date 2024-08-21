039ec5e52fe19d274a338e722a74564369795599
Michal Sykacek <sykyman0000@gmail.com>
2024-08-21 11:23:46 +0200

    update gitignore + fix gnuplot

--- src/buildingFiles/gnuplot.sh
+++ src/buildingFiles/gnuplot.sh
@@ -8,6 +8,6 @@ do for[t in system("ls ../out/data/*.dat | cut -d/ -f4")]{
     set title 'Sound Pressure Level - time'
     set xlabel 't [s]'
     set ylabel 'SPL [dB]'
-    plot '../out/data/'.t w l title 'soubor '.t
+    plot '../out/data/'.t w l title 'file '.t
 
 }
