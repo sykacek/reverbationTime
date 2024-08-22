2d817df92209be3c6b6bd9fa8e548d1f4f40abfd
Michal Sykacek <sykyman0000@gmail.com>
2024-08-22 22:03:18 +0200

    Update build scripts

--- src/buildf/mvsrc.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-for file in src/audio_datcp/*
-do  
-    var=$(echo "$file" | cut -d/ -f3)
-#    echo $var
-    mv $file output/data/$var
-done
-
--- src/buildf/rename.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-for file in output/images/*.dat.png
-do  
-    var=${file%.*}
-#    echo ${var%.*}.png
-    mv $file ${var%.*}.png
-done
